ea951b324199aa4114ac13f5e1c5680e56d45a88
Osmar Leandro <osmarleandro@ufcg.edu.br>
2019-06-12 17:12:12 -0300

    [fix] new route like, resizing img

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
+
 const { user, pass } = require("./config/db-credentials.json");
 
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 mongoose.connect(
   `mongodb+srv://${user}:${pass}@cluster0-1wjgy.mongodb.net/test?retryWrites=true&w=majority`,
   { useNewUrlParser: true }
 );
 
+app.use(cors());
 app.use(require("./routes"));
+app.use(
+  "/files",
+  express.static(path.resolve(__dirname, "..", "uploads", "resized"))
+);
 
-app.listen(3000);
+server.listen(3000);
